c31bebdbab8a8bb1bb05f8bc03fe343989b50b50
Brian Weaver <bdrums@gmail.com>
2020-01-10 16:44:29 -0500

    Clean up routes file a bit

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,29 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  get "/menu", to: "menu#new"
-  resources :meals
   namespace :admin do
-    resources :users
     resources :announcements
     resources :notifications
     resources :services
+    resources :users
 
     root to: "users#index"
   end
+
+  authenticate :user, lambda { |u| u.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
+
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+
+  get "/menu", to: "menu#new"
   get '/privacy', to: 'home#privacy'
   get '/terms', to: 'home#terms'
-    authenticate :user, lambda { |u| u.admin? } do
-      mount Sidekiq::Web => '/sidekiq'
-    end
 
-
-  resources :notifications, only: [:index]
   resources :announcements, only: [:index]
-  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+  resources :meals
+  resources :notifications, only: [:index]
+
   root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
