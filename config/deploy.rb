1fdeb3c28b439a575ba5a1d819f27a619500ae7d
Brian Weaver <bdrums@gmail.com>
2020-05-18 08:27:01 -0400

    Whole lotta rosie

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.2"
 
-set :application, "weekly_meals"
+set :application, "router_admin"
 set :repo_url, "git@github.com:bweave/weekly_meals.git"
 
 # Default branch is :master
@@ -10,22 +10,22 @@ set :repo_url, "git@github.com:bweave/weekly_meals.git"
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/home/pi/#{fetch :application}"
 
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", ".bundle", "public/system", "public/uploads"
 
 # Only keep the last 3 releases to save disk space
 set :keep_releases, 3
 
 # Optionally, you can symlink your database.yml and/or secrets.yml file from the shared directory during deploy
-# This is useful if you don't want to use ENV variables
-# append :linked_files, 'config/database.yml', 'config/secrets.yml'
+# This is useful if you don"t want to use ENV variables
+# append :linked_files, "config/database.yml", "config/secrets.yml"
 
 namespace :deploy do
-  desc 'Reload Sidekiq'
-  task :reload_sidekiq do
-    on roles(:app) do
-      execute "sudo service sidekiq reload"
-    end
-  end
+  # desc "Reload Sidekiq"
+  # task :reload_sidekiq do
+    # on roles(:app) do
+      # execute "sudo service sidekiq reload"
+    # end
+  # end
 end
 
-after 'deploy:publishing', 'deploy:reload_sidekiq'
+# after "deploy:publishing", "deploy:reload_sidekiq"
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server "rpi4.local", user: "pi", roles: %w(app db web)
+server "bweave.local", user: "pi", roles: %w(app db web)
 
 # role-based syntax
 # ==================
